8c6f0d9d2d66432504c9da3d288f3a846462b250
Roche <rochej@gmail.com>
2016-03-20 15:58:42 -0700

    Update params parsing

--- app/controllers/flic_controller.rb
+++ app/controllers/flic_controller.rb
@@ -1,6 +1,6 @@
 post '/flic' do
   p params
-  fliced = params.split(",")
+  fliced = params.first.key.split(",")
   @user = User.find_by("name=?", fliced[0])
   @chore = chore.find_by("name=?", fliced[1])
   @user.chores << @chore
