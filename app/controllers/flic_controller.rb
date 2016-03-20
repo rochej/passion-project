9e825cdf373f1661075c9c7ce1c084e64180b85a
Roche <rochej@gmail.com>
2016-03-20 16:03:31 -0700

    Fix spelling of chore

--- app/controllers/flic_controller.rb
+++ app/controllers/flic_controller.rb
@@ -2,7 +2,7 @@ post '/flic' do
   p params
   fliced = params.keys[0].split(",")
   @user = User.find_by("name=?", fliced[0])
-  @chore = chore.find_by("name=?", fliced[1])
+  @chore = Chore.find_by("name=?", fliced[1])
   @user.chores << @chore
   p @user.chores
 end
