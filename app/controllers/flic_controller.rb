cb00745df53b90801c3bfdbed74fe6657ea4e3bd
Roche <rochej@gmail.com>
2016-03-20 15:54:25 -0700

    Update flic controller

--- app/controllers/flic_controller.rb
+++ app/controllers/flic_controller.rb
@@ -1,3 +1,8 @@
 post '/flic' do
   p params
+  fliced = params.split(",")
+  @user = User.find_by("name=?", fliced[0])
+  @chore = chore.find_by("name=?", fliced[1])
+  @user.chores << @chore
+  p @user.chores
 end
