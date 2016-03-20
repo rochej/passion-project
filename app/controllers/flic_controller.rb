50367d4a9a8cb5ebccc9d298fadb76831844a36c
Roche <rochej@gmail.com>
2016-03-20 16:01:45 -0700

    Again with the params

--- app/controllers/flic_controller.rb
+++ app/controllers/flic_controller.rb
@@ -1,6 +1,6 @@
 post '/flic' do
   p params
-  fliced = params.first.key.split(",")
+  fliced = params.keys[0].split(",")
   @user = User.find_by("name=?", fliced[0])
   @chore = chore.find_by("name=?", fliced[1])
   @user.chores << @chore
